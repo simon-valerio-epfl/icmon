37ed450d53580f892171a670db9f59b5470b1988
Androz2091 <androz2091@gmail.com>
2023-12-05 16:36:33 +0100

    page 33

--- icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/fights/ICMonFight.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/gamelogic/fights/ICMonFight.java
@@ -6,24 +6,28 @@ import ch.epfl.cs107.icmon.graphics.ICMonFightArenaGraphics;
 import ch.epfl.cs107.icmon.graphics.ICMonFightTextGraphics;
 import ch.epfl.cs107.play.engine.PauseMenu;
 import ch.epfl.cs107.play.window.Canvas;
+import ch.epfl.cs107.play.window.Keyboard;
 
 public class ICMonFight extends PauseMenu {
 
-    private float counter = 5f;
+    private enum FightState { INTRODUCTION, COUNTER, CONCLUSION, ENDED };
+
+    private float timeCounter = 5f;
 
     private Pokemon playerPokemon;
     private Pokemon opponent;
 
     private ICMonFightArenaGraphics arena;
+    private FightState state = FightState.INTRODUCTION;
+    private ICMonFightTextGraphics introductionTextGraphics = new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "Welcome to the fight");
+    private ICMonFightTextGraphics conclusionTextGraphics = new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "Good fight!");
 
     public ICMonFight(Pokemon playerPokemon, Pokemon opponent) {
         this.playerPokemon = playerPokemon;
         this.opponent = opponent;
 
         this.arena = new ICMonFightArenaGraphics(CAMERA_SCALE_FACTOR, playerPokemon.properties(), opponent.properties());
-        this.arena.setInteractionGraphics(
-                new ICMonFightTextGraphics(CAMERA_SCALE_FACTOR, "Hello World")
-        );
+        this.arena.setInteractionGraphics(introductionTextGraphics);
     }
 
     @Override
@@ -33,10 +37,39 @@ public class ICMonFight extends PauseMenu {
 
     public void update(float deltaTime) {
         super.update(deltaTime);
-        counter -= deltaTime;
+
+        switch (this.state) {
+            case INTRODUCTION -> {
+                this.arena.setInteractionGraphics(introductionTextGraphics);
+
+                Keyboard keyboard = getKeyboard();
+                if (keyboard.get(Keyboard.SPACE).isDown()) {
+                    this.state = FightState.COUNTER;
+                }
+
+            }
+            case CONCLUSION -> {
+                this.arena.setInteractionGraphics(conclusionTextGraphics);
+
+                Keyboard keyboard = getKeyboard();
+                if (keyboard.get(Keyboard.SPACE).isDown()) {
+                    this.state = FightState.ENDED;
+                }
+            }
+            case COUNTER -> {
+                timeCounter -= deltaTime;
+                if (timeCounter <= 0) {
+                    state = FightState.CONCLUSION;
+                }
+                System.out.println(timeCounter);
+            }
+            default -> {
+                // do nothing
+            }
+        }
     }
 
     public boolean isRunning() {
-        return counter > 0;
+        return !(this.state == FightState.ENDED);
     }
 }
