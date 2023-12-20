5d13ed3ad7d48ef0b23ce8ff5766d45884d7b9cf
Androz2091 <androz2091@gmail.com>
2023-12-20 20:19:55 +0100

    add new doc

--- icmon/src/main/java/ch/epfl/cs107/icmon/audio/ICMonSoundManager.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/audio/ICMonSoundManager.java
@@ -9,6 +9,12 @@ import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 import java.io.InputStream;
 
+/**
+ * Represents the sound manager of the game.
+ *
+ * @author Valerio De Santis
+ * @author Simon Lefort
+ */
 public class ICMonSoundManager {
     private final FileSystem fileSystem;
     private Clip currentClip;
@@ -19,10 +25,36 @@ public class ICMonSoundManager {
     private boolean resetRequired = false;
     private LineListener stopListener;
 
+    /**
+     * Creates a new game sound manager
+     *
+     * @param fileSystem game's file system
+     */
     public ICMonSoundManager (FileSystem fileSystem) {
         this.fileSystem = fileSystem;
     }
 
+    /**
+     * Creates a SwingSound from a given sound name
+     * @param soundName the name of the sound to read
+     * @return the SwingSound
+     */
+    private SwingSound readSwingSound(String soundName) {
+        try {
+            InputStream inputStream = fileSystem.read(ResourcePath.getSound(soundName));
+            return new SwingSound(inputStream);
+        } catch (Exception e) {
+            System.out.println("Can not read sound... " + soundName);
+            return null;
+        }
+    }
+
+    /**
+     * Stops the currently played sound if there is one
+     * and resets the sound manager state
+     * this method shall be called just before a new sound is starting
+     * or when the current sound has ended
+     */
     public void resetSound () {
         resetRequired = false;
         if (currentClip != null) {
@@ -35,76 +67,92 @@ public class ICMonSoundManager {
         currentPlayingSound = "N/A";
     }
 
+    /**
+     * Plays a sound for a given duration
+     *
+     * @param name the name of the sound to play
+     * @param duration the duration of the sound in frames
+     */
     public void playSound (String name, int duration) {
         playSound(name, duration, false);
     }
 
+    /**
+     * Plays a sound for a given duration
+     *
+     * @param name the name of the sound to play
+     * @param duration the duration of the sound in frames
+     * @param isPrioritySound whether this sound should replace the currently playing sound if there is one
+     */
     public void playSound (String name, int duration, boolean isPrioritySound) {
-        try {
-            InputStream inputStream = fileSystem.read(ResourcePath.getSound(name));
-            SwingSound sound = new SwingSound(inputStream);
 
-            // we reset the time left if the sound is already playing
-            if (this.currentPlayingSound.equals(name)) {
-                timeLeft = duration;
-                return;
-            }
+        SwingSound sound = readSwingSound(name);
+        if (sound == null) return;
 
-            if (isPlayingPrioritySound && !isPrioritySound) {
-                return;
-            }
-
-            resetSound();
-
-            resetRequired = true;
-
-            isPlayingPrioritySound = isPrioritySound;
-
-            Clip clip = sound.openedClip(0);
-            if (clip != null) {
-                clip.start();
-
-                LineListener listener = event -> {
-                    if (event.getType() == LineEvent.Type.STOP) {
-                        resetSound();
-                    }
-                };
-
-                // detect when file ends
-                clip.addLineListener(listener);
-                stopListener = listener;
-            }
-            this.currentPlayingSound = name;
-            this.timeLeft = duration;
-            this.currentClip = clip;
-
-        } catch (Exception e) {
-            System.out.println("Can not play sound... " + name);
+        // if the same sound is already playing
+        // just extend its duration
+        if (this.currentPlayingSound.equals(name)) {
+            timeLeft = duration;
+            return;
         }
+
+        // if the given sound is not priority and a priority sound is already playing
+        // skip it
+        if (isPlayingPrioritySound && !isPrioritySound) return;
+        isPlayingPrioritySound = isPrioritySound;
+
+        // reset all the currently playing sound
+        resetSound();
+
+        resetRequired = true;
+        Clip clip = sound.openedClip(0);
+        if (clip != null) {
+            clip.start();
+
+            LineListener listener = event -> {
+                if (event.getType() == LineEvent.Type.STOP) {
+                    resetSound();
+                }
+            };
+
+            // detect when file ends
+            clip.addLineListener(listener);
+            stopListener = listener;
+        }
+        this.currentPlayingSound = name;
+        this.timeLeft = duration;
+        this.currentClip = clip;
     }
 
+    /**
+     * Plays a background sound
+     * @param name the name of the sound to play
+     */
     public void playBackgroundSound (String name) {
-        try {
-            InputStream inputStream = fileSystem.read(ResourcePath.getSound(name));
-            SwingSound sound = new SwingSound(inputStream);
+        SwingSound sound = readSwingSound(name);
+        if (sound == null) return;
 
-            Clip clip = sound.openedClip(0);
-            if (clip != null) {
-                clip.loop(Clip.LOOP_CONTINUOUSLY);
-            }
-            this.backgroundClip = clip;
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Can not play background sound... " + name);
+        Clip clip = sound.openedClip(0);
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
         }
+        this.backgroundClip = clip;
     }
 
+    /**
+     * Stops the currently playing background sound
+     * if there is one
+     */
     public void resetBackgroundSound () {
         if (backgroundClip != null) {
             backgroundClip.stop();
         }
     }
 
+    /**
+     * Updates the sound manager state
+     * this method shall be called by the game
+     */
     public void update() {
         if (timeLeft < 0 && resetRequired) {
             resetSound();
